fd63f5a74c340de8a44a27f30a52c2c1c0fe901a
bazil777 <95894207+bazil777@users.noreply.github.com>
2023-11-04 20:52:41 +0000

    prototype
    new repo
    improved controls

---
+++ Assets/Scripts/Enemy AI.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyAI : MonoBehaviour
+{
+    public GameObject target; // Change this from Transform to GameObject
+
+    public float chaseRange = 15f;
+    public float moveSpeed = 3.0f;
+    public float contactRange = 1.0f; // Define the contact range for the enemy.
+    public float attackCooldown = 2.0f; // Time to wait between attacks.
+
+    public Material chasingMaterial; // Assign chasing in the Inspector.
+
+    public int damageAmount = 5;
+    public int maxHealth = 50; // Maximum health of the enemy
+
+    private Material originalMaterial;
+    private Renderer coneRenderer;
+    private bool canAttack = true;
+    private int currentHealth; // Current health of the enemy
+
+    public PlayerGold playerGold; // Reference to the PlayerGold script
+
+    void Start()
+    {
+        // Initialize the coneRenderer and originalMaterial.
+        coneRenderer = GetComponent<Renderer>();
+        originalMaterial = coneRenderer.material;
+        currentHealth = maxHealth; // Set the current health to the maximum health at the start.
+    }
+
+    void Update()
+    {
+        if (target == null) return;
+
+        float distanceToPlayer = Vector3.Distance(transform.position, target.transform.position);
+
+        if (distanceToPlayer <= chaseRange)
+        {
+            coneRenderer.material = chasingMaterial;
+            Vector3 direction = (target.transform.position - transform.position).normalized;
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+
+            // Check if the enemy is in contact with the player and can attack.
+            if (distanceToPlayer < contactRange && canAttack)
+            {
+                // Deal damage to the player.
+                target.GetComponent<PlayerHealth>().TakeDamage(damageAmount);
+
+                // Start the attack cooldown.
+                StartCoroutine(AttackCooldown());
+            }
+        }
+        else
+        {
+            coneRenderer.material = originalMaterial;
+        }
+    }
+
+    IEnumerator AttackCooldown()
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+       
+        // Destroy the enemy GameObject.
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Magic Ball.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class MagicBall : MonoBehaviour
+{
+    public float healingRadius = 3f; // Radius within which the player is healed
+    public int healingAmount = 5; // Amount of health to restore on contact
+    public int goldDeductionAmount = 10; // Amount of gold to deduct on contact
+    public float healingInterval = 1f; // Time interval between healing (adjust as needed)
+
+    private float nextHealTime;
+
+    private void Start()
+    {
+        nextHealTime = Time.time + healingInterval;
+    }
+
+    private void Update()
+    {
+        if (Time.time >= nextHealTime)
+        {
+            // Find all colliders within the healing radius
+            Collider[] colliders = Physics.OverlapSphere(transform.position, healingRadius);
+
+            foreach (Collider collider in colliders)
+            {
+                // Check if the collider belongs to the player
+                PlayerHealth playerHealth = collider.GetComponent<PlayerHealth>();
+                PlayerGold playerGold = collider.GetComponent<PlayerGold>();
+
+                if (playerHealth != null)
+                {
+                    // Heal the player if they are in the healing radius
+                    playerHealth.Heal(healingAmount);
+
+                    if (playerGold != null)
+                    {
+                        // Deduct gold from the player
+                        playerGold.DeductGold(goldDeductionAmount);
+                    }
+                }
+            }
+
+            // Set the time for the next healing event
+            nextHealTime = Time.time + healingInterval;
+        }
+    }
+}
